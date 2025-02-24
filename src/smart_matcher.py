80c378b5629f3c3bff5406a358527447ef8fad70
beatriz-farias <beatrizfarias.engcomp@gmail.com>
2025-02-24 08:51:51 -0300

    update: smart_matcher.py

--- src/smart_matcher.py
+++ src/smart_matcher.py
@@ -8,10 +8,6 @@ def job_resume_matcher(job_description_path, resume_path):
     
     job_description_clean = preprocess_text(job_description_text)
     resume_clean = preprocess_text(resume_text)
-    print('job description')
-    print(job_description_clean)
-    print('resume')
-    print(resume_clean)
     similarity_score = compute_similarity(job_description_clean, resume_clean)
     
     return similarity_score
